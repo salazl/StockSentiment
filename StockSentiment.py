adcd599e8dde4c2ac57bee16394a89d386ae6cbe
Luis <91570846+salazl@users.noreply.github.com>
2021-10-14 17:28:04 -0500

    updated stocksentiment.py calls + questions
    Added new function names for paired_general_search(), paired_username_search(), unpaired_general_search(), unpaired_username_search(). 
    
    Also updated questions to determine which function is run for user

--- StockSentiment.py
+++ StockSentiment.py
@@ -1,16 +1,35 @@
+from StockSentiment_paired import paired_general_search, paired_username_search
+from StockSentiment_unpaired import unpaired_general_search, unpaired_username_search
 
-from StockSentiment_functions import general_search, username_search #import functions from other file
-
-function_picker = ['general', 'username']
-function_choice = None
+pair_function_picker = ['paired', 'unpaired']
+pair_function_choice = None
 
 #asking the user which search type they want to perform
-while function_choice not in function_picker:
-        function_choice = input('Would you like to perform a '+"'"+'general'+"'"+' or a '+"'"+'username'+"'"+' search?')
+while pair_function_choice not in pair_function_picker:
+        pair_function_choice = input('Would you like to perform a '+"'"+'paired'+"'"+' or '+"'"+'unpaired'+"'"+' search?')
 
-if function_choice == 'general':
-    general_search()
+if pair_function_choice == 'paired':
+    type_function_picker = ['general', 'username']
+    type_function_choice = None
+    
+    while type_function_choice not in type_function_picker:
+        type_function_choice = input('Would you like to perform a '+"'"+'general'+"'"+' or a '+"'"+'username'+"'"+' search?')
+        
+    if type_function_choice == 'general':
+        paired_general_search()
+        
+    elif type_function_choice = 'username':
+        paired_username_search()
 
-elif function_choice == 'username':
-    username_search()
-
+elif pair_function_choice == 'unpaired':
+    type_function_picker = ['general', 'username']
+    type_function_choice = None
+    
+    while type_function_choice not in type_function_picker:
+        type_function_choice = input('Would you like to perform a '+"'"+'general'+"'"+' or a '+"'"+'username'+"'"+' search?')
+        
+    if type_function_choice == 'general':
+        unpaired_general_search()
+        
+    elif type_function_choice = 'username':
+        unpaired_username_search()
